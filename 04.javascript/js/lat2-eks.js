8e6216df122dc1294869792ddc8f96ee36361382
fatiya17 <fatiyalabibah17@gmail.com>
2026-03-10 13:31:18 +0700

    add latihan javascript praktikum

---
+++ 04.javascript/js/lat2-eks.js
@@ -0,0 +1,6 @@
+alert("Assalamualaikum");
+alert("Selamat Datang di Latihan JavaScript Internal");
+alert("Silakan Diakses dengan Baik");
+document.write("<h1>Latihan JavaScript Internal</h1>");
+
+console.log("Ini adalah latihan JavaScript Internal");
