a309fb3bfdfa795adaf201614103821447ead9cf
fatiya17 <fatiyalabibah17@gmail.com>
2026-03-31 09:31:54 +0700

    praktikum 12 modul named dan default export app

---
+++ 04.javascript/modul/gabungan_app.js
@@ -0,0 +1,6 @@
+import multykali, { pi, tambah } from './gabungan_math.js';
+
+console.log(multykali(5, 5));
+console.log(`nilai pi : ${pi}`);
+console.log(`hasil tambah : ${tambah(10, 20)}`);
+console.log(`hasil kali : ${multykali(10, 20)}`);
