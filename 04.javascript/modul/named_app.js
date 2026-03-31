b09217856decf6fbdbb27c596f4d905bd1fb1cfc
fatiya17 <fatiyalabibah17@gmail.com>
2026-03-31 09:31:07 +0700

    praktikum 12 modul named export app

---
+++ 04.javascript/modul/named_app.js
@@ -0,0 +1,5 @@
+import {pi, tambah, kurang} from './named_math.js';
+
+console.log(`nilai pi : ${pi}`);
+console.log(`hasil tambah : ${tambah(10, 20)}`);
+console.log(`hasil kurang : ${kurang(10, 20)}`);
